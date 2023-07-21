b2f34ac9d7ad7ba2deb7ef17518b6a4aec3678e1
dashixiong <liwenwu@mobiustech.cn>
2023-07-21 20:27:58 +0800

    taohualing340

---
+++ js/taohualing340.js
@@ -0,0 +1,43 @@
+const baseUrl = 'assets/';
+const viewer = new PhotoSphereViewer.Viewer({
+  container: document.querySelector('#viewer'),
+  panorama: baseUrl + 'panorama/taohualing-340m-20230721.jpg',
+  caption: '桃花岭山脊步道 . 340米（2023/07/21）',
+  touchmoveTwoFingers: true,
+  navbar: ['zoom', 'caption', 'fullscreen'],
+  plugins: [
+    [PhotoSphereViewer.MarkersPlugin, {
+      markers: [
+        {
+          id: 'cstip-xiyaxiaoxue',
+          tooltip: {
+            content: document.querySelector('#cstip-taohualing-content').innerHTML,
+            className: 'custom-tooltip',
+            position: 'top',
+            trigger: 'hover',
+          },
+          position: { pitch: -0.5, yaw: -0.15 },
+          image: baseUrl + 'images/pictos/pin-blue.png',
+          size: { width: 48, height: 48 },
+          anchor: 'bottom center',
+          hoverScale: { amount: 1.5, easing: 'ease-in-out', duration: 500 },
+        }
+      ],
+    }],
+  ],
+
+});
+
+const markersPlugin = viewer.getPlugin(PhotoSphereViewer.MarkersPlugin);
+viewer.addEventListener('ready', () => {
+  viewer
+    .animate({
+      yaw: 0,
+      pitch: -0.3,
+      speed: 1000,
+    })
+    .then(() => {
+      //markersPlugin.showMarkerTooltip('custom-tooltip');
+    });
+}, { once: true });
+
